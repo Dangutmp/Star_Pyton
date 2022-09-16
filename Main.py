e8066a9a92e858c432d5cb8930c0f27c43b06354
Dangutmp <85625618+Dangutmp@users.noreply.github.com>
2022-09-16 11:17:06 +0300

    buil the simlest calculator

--- Main.py
+++ Main.py
@@ -1,1 +1,13 @@
-print("hellow")
+print("hellow\n")
+print("input two number and mathematic opperationor it")
+x1 = float(input())
+operation = input()
+x2 = float(input())
+if operation == "+":
+    print(x1 + x2)
+elif operation == "-":
+    print(x1 - x2)
+elif operation == "*":
+    print(x1 * x2)
+elif operation == "/":
+    print1(x1 / x2)
